8d6f1a70404d394589b0d8b41f03084800e2608f
Diogo Cardinot <diogocardinotuerj@gmail.com>
2022-03-03 20:25:13 -0300

    Cria o header desktop e mobile sem funcionalidade

---
+++ desafio-react-ts/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Header } from "./components/Header";
+import { Newsletter } from "./components/Newsletter";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Newsletter />
+    </div>
+  );
+}
+
+export default App;
