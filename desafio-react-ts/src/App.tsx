814ee07ea73b22fdc13f5545fc5e99e9c8893bab
Diogo Cardinot <diogocardinotuerj@gmail.com>
2022-03-04 20:57:13 -0300

    Cria o footer links desktop

--- desafio-react-ts/src/App.tsx
+++ desafio-react-ts/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import { Header } from "./components/Header/Header";
 import { Newsletter } from "./components/Newsletter/Newsletter";
+import { FooterLinksDesktop } from "./components/FooterLinks/FooterLinksDesktop";
 
 function App() {
   return (
     <div>
       <Header />
       <Newsletter />
+      <FooterLinksDesktop />
     </div>
   );
 }
