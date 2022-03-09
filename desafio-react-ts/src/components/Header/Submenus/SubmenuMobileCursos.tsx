e222984bea087e61d9a434a0b46a70fa02a66bf3
Diogo Cardinot <diogocardinotuerj@gmail.com>
2022-03-09 17:14:49 -0300

    Adiciona algumas validacoes ao formulario

--- desafio-react-ts/src/components/Header/Submenus/SubmenuMobileCursos.tsx
+++ desafio-react-ts/src/components/Header/Submenus/SubmenuMobileCursos.tsx
@@ -4,8 +4,6 @@ import "./SubmenuMobileCursos.css";
 const SubMenuMobileCursos = () => {
   return (
     <div className="submenu-mobile-cursos">
-      <h2 className="submenu-mobile-cursos-title">Cursos</h2>
-
       <ul className="submenu-mobile-cursos-links">
         <li className="submenu-mobile-cursos-link">Lorem Ipsum</li>
         <li className="submenu-mobile-cursos-link">Lorem Ipsum</li>
--- desafio-react-ts/src/components/Header/Submenus/SubmenuMobileSaibaMais.tsx
+++ desafio-react-ts/src/components/Header/Submenus/SubmenuMobileSaibaMais.tsx
@@ -4,8 +4,6 @@ import "./SubmenuMobileSaibaMais.css";
 const SubMenuMobileSaibaMais = () => {
   return (
     <div className="submenu-mobile-saiba-mais">
-      <h2 className="submenu-mobile-saiba-mais-title">Saiba Mais</h2>
-
       <ul className="submenu-mobile-saiba-mais-links">
         <li className="submenu-mobile-saiba-mais-link">Lorem Ipsum</li>
         <li className="submenu-mobile-saiba-mais-link">Lorem Ipsum</li>
